e62998c8b32df36fcb5ffffc4cd0e652580efe42
Netikaz <Netikaz.Tek@gmail.com>
2014-01-22 15:55:10 +0100

    Bugfix

---
+++ WinMediaPLayer/PlayListLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinMediaPLayer.Models;
+
+namespace WinMediaPLayer
+{
+    class PlayListLoader
+    {
+        private List<PlaylistModel> plBase = new List<PlaylistModel>();
+
+        public PlayListLoader()
+        {
+            string[] filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\Playlists", "*.bmp");
+            for (int it = 0; filePaths[it] != null; it++)
+            {
+                MessageBox.Show(filePaths.ToString());
+            }
+            
+        }
+    }
+}
